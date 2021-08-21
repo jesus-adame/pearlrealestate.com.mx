cdb923a45741b207716a507275792b019dd589bf
Jesus Adame <jesus.adame@ancient.mx>
2021-08-20 21:17:58 -0500

    feat: Improve home page layout

---
+++ resources/views/layouts/carousel.blade.php
@@ -0,0 +1,56 @@
+<div class="mx-auto w-full pt-16 md:pt-0" style="max-width: 3000px">
+    <div class="splide w-full">
+        <div class="splide__track">
+            <ul class="splide__list">
+                <li class="splide__slide">
+                    <div class="overflow-hidden relative h-full" style="min-height: 400px">
+                        <img class="filter h-full w-full brightness-50" src="/images/slide5.jpg" alt="Pearl Estate">
+                        <div class="absolute top-0 bottom-0 right-0 left-0 m-auto w-full max-w-7xl h-52 p-5">
+                            <p class="text-4xl text-gray-50 uppercase mb-4" style="text-shadow: 1px 2px 2px #000">
+                                Encuentra tu lugar ideal
+                            </p>
+                            <p class="text-white w-96" style="text-shadow: 1px 2px 2px #000">
+                                Somos una empresa dedicada en vender mas que solo metros cuadrados,
+                                nos enfocamos en darte la mejor opción en distribución arquitectónica, según sea sus
+                                distintas necesidades
+                            </p>
+                            <a href="#properties" class="rounded-sm mt-6 py-2 px-6 font-semibold hover:bg-yellow-500 bg-yellow-400 inline-block uppercase text-center shadow-md transition duration-300">Conoce más</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="splide__slide">
+                    <div class="overflow-hidden relative h-full" style="min-height: 400px">
+                        <img class="filter h-full w-full brightness-50" src="/images/slide1.jpg" alt="Pearl Estate">
+                        <div class="absolute top-0 bottom-0 right-0 left-0 m-auto w-full max-w-7xl h-52 p-5">
+                            <p class="text-4xl text-gray-50 uppercase mb-4" style="text-shadow: 1px 2px 2px #000">
+                                Encuentra tu lugar ideal
+                            </p>
+                            <p class="text-white w-96" style="text-shadow: 1px 2px 2px #000">
+                                Somos una empresa dedicada en vender mas que solo metros cuadrados,
+                                nos enfocamos en darte la mejor opción en distribución arquitectónica, según sea sus
+                                distintas necesidades
+                            </p>
+                            <a href="#properties" class="rounded-sm mt-6 py-2 px-6 font-semibold hover:bg-yellow-500 bg-yellow-400 inline-block uppercase text-center shadow-md transition duration-300">Conoce más</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="splide__slide">
+                    <div class="overflow-hidden relative h-full" style="min-height: 400px">
+                        <img class="filter h-full w-full brightness-50" src="/images/slider1.jpg" alt="Pearl Estate">
+                        <div class="absolute top-0 bottom-0 right-0 left-0 m-auto w-full max-w-7xl h-52 p-5">
+                            <p class="text-4xl text-gray-50 uppercase mb-4" style="text-shadow: 1px 2px 2px #000">
+                                Encuentra tu lugar ideal
+                            </p>
+                            <p class="text-white w-96" style="text-shadow: 1px 2px 2px #000">
+                                Somos una empresa dedicada en vender mas que solo metros cuadrados,
+                                nos enfocamos en darte la mejor opción en distribución arquitectónica, según sea sus
+                                distintas necesidades
+                            </p>
+                            <a href="#properties" class="rounded-sm mt-6 py-2 px-6 font-semibold hover:bg-yellow-500 bg-yellow-400 inline-block uppercase text-center shadow-md transition duration-300">Conoce más</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="container mx-auto">
         <section>
-            <h1 class="pt-20 text-gray-700">Propiedades</h1>
+            <h1 class="pt-24 text-gray-700">Propiedades</h1>
             <br>
             <hr>
             <br>
