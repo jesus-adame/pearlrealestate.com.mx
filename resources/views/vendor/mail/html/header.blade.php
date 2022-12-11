7fe94c5dad10f456a4c58fbdf4cddb68ff31b4a7
Jesus Adame <jadame@osmos.mx>
2022-12-10 22:35:48 -0600

    Fix header mails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://pearlrealestate.com.mx/images/logo_pearl_2.png" class="logo" alt="Pearl Real Estate Logo">
+    <img src="https://pearlrealestate.com.mx/images/logo_pearl_2.png" class="logo" alt="Pearl Real Estate Logo">
 @else
-{{ $slot }}
+    <img src="https://pearlrealestate.com.mx/images/logo_pearl_2.png" class="logo" alt="Pearl Real Estate Logo">
 @endif
 </a>
 </td>
